383944747270492296634d9fd611a4856fb13efa
Dimas R Ismail <dimas@akhdani.co.id>
2023-04-04 22:24:17 +0700

    refactor: delete validation.ts

--- src/util/validation.ts
+++
@@ -1,6 +0,0 @@
-import { getExtension } from '@util/helpers';
-
-export const isValidExtension = (filename: string, allowedExtension: string) => {
-  const extension = getExtension(filename);
-  return extension === allowedExtension;
-}
