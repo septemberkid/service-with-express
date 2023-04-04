a4401e6d577bfbf43e692c1186fc9244f617a845
Dimas R Ismail <dimas@akhdani.co.id>
2023-04-04 20:35:59 +0700

    refactor: test-case path-prefixer.spec.ts and delete date-time.test.ts, encryptor.test.ts

--- src/__test__/date-time.test.ts
+++
@@ -1,8 +0,0 @@
-import { generateTokenExpired } from '@util/date-time';
-
-describe('date-time', () => {
-  test('token-expired-time', () => {
-    const exp = generateTokenExpired(3600);
-    console.log(exp)
-  })
-})
